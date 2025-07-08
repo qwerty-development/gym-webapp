905fec6abc923f8559da05be7ebff900409684a6
charbellazzi1 <charbel.azzy@hotmail.com>
2025-07-08 12:22:25 +0300

    fixed

--- utils/userRequests/activitiesRequests.js
+++ utils/userRequests/activitiesRequests.js
@@ -30,7 +30,8 @@ export const fetchAllActivitiesGroup = async () => {
 	const { data, error } = await supabase
 		.from('activities')
 		.select('id, name, credits, capacity') // Ensure 'credits' is included
-		.eq('group', true)
+		.or('group.eq.true,semi_private.eq.true')
+		.neq('name', 'Sunrise Circuit 7')
 
 	if (error) {
 		console.error('Error fetching activities:', error.message)
